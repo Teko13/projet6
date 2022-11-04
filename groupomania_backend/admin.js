f7782bb2055f60d249848948c3c8593c21b42bd6
teko folly <ffabrice999@gmail.com>
2022-11-04 22:28:32 +0100

    correct routing bug

--- groupomania_backend/admin.js
+++ groupomania_backend/admin.js
@@ -1,12 +1,9 @@
 module.exports = (req, res, next) => {
     try {
-<<<<<<< HEAD
         if (!req.headers.isAdmin) {
-=======
-        if (!req.header.isAdmin) {
->>>>>>> back
             return res.status(401).json({ message: "Acces non autorisé" })
         }
+        console.log('admin valider');
         next();
     } catch (error) {
         res.status(400).json({ error });
